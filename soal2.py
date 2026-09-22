a69a24a6653d7104fe4676ec63c0607738c46fb3
raffibromabar-web <raffibromabar@gmail.com>
2026-09-22 11:34:26 +0700

    mencari angka terbesar yang angka3

--- soal2.py
+++ soal2.py
@@ -9,3 +9,7 @@ if angka1 > angka2 and angka1 > angka3:
 elif angka2 > angka1 and angka2 > angka3:
     terbesar = angka2
     print ("angka terbesar :", terbesar)
+
+elif angka3 > angka1 and angka3 > angka2:
+    terbesar = angka3
+    print ("angka terbesar :", terbesar)
