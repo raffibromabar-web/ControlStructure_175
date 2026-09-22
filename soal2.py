5d169d8b04f37465c3a30218f15f6795292f8fdc
raffibromabar-web <raffibromabar@gmail.com>
2026-09-22 11:32:57 +0700

    untuk perbandingan angka 1,2,3 dan terbesar angka1

--- soal2.py
+++ soal2.py
@@ -1,3 +1,7 @@
 angka1 = float(input("Masukkan angka pertama: "))
 angka2 = float(input("Masukkan angka kedua: "))
 angka3 = float(input("Masukkan angka ketiga: "))
+
+if angka1 > angka2 and angka1 > angka3:
+    terbesar = angka1
+    print ("angka terbesar :", terbesar)
