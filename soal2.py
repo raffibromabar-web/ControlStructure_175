ae572e46a0a0e9731ccefac68dbae8f0d17aebbe
raffibromabar-web <raffibromabar@gmail.com>
2026-09-22 11:33:39 +0700

    untuk mencari angka terbesar yang angka 2

--- soal2.py
+++ soal2.py
@@ -5,3 +5,7 @@ angka3 = float(input("Masukkan angka ketiga: "))
 if angka1 > angka2 and angka1 > angka3:
     terbesar = angka1
     print ("angka terbesar :", terbesar)
+
+elif angka2 > angka1 and angka2 > angka3:
+    terbesar = angka2
+    print ("angka terbesar :", terbesar)
